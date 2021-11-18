0e0dc934ff7628614db7ffeb6033aa7d0c4b5a26
bran.liang <bran.liang@shopify.com>
2021-11-18 22:19:41 +0800

    Add deploy script

---
+++ ubuntu/deploy.sh
@@ -0,0 +1,45 @@
+# https://gorails.com/deploy/ubuntu/20.04
+
+# Adding Node.js repository
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+# Adding Yarn repository
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo add-apt-repository ppa:chris-lea/redis-server
+
+# Refresh our packages list with the new repositories
+sudo apt-get update
+
+# Install our dependencies for compiiling Ruby along with Node.js and Yarn
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev dirmngr gnupg apt-transport-https ca-certificates redis-server redis-tools nodejs yarn
+
+# Install Ruby
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+git clone https://github.com/rbenv/rbenv-vars.git ~/.rbenv/plugins/rbenv-vars
+exec $SHELL
+rbenv install 3.0.2
+rbenv global 3.0.2
+ruby -v
+
+# This installs the latest Bundler, currently 2.x.
+gem install bundler
+bundle -v
+
+# Config web server
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger focal main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+sudo apt-get install -y nginx-extras libnginx-mod-http-passenger
+if [ ! -f /etc/nginx/modules-enabled/50-mod-http-passenger.conf ]; then sudo ln -s /usr/share/nginx/modules-available/mod-http-passenger.load /etc/nginx/modules-enabled/50-mod-http-passenger.conf ; fi
+sudo ls /etc/nginx/conf.d/mod-http-passenger.conf
+
+# Config passenger
+# Config nginx
+
+# PostgreSQL
+sudo apt-get install postgresql postgresql-contrib libpq-dev
